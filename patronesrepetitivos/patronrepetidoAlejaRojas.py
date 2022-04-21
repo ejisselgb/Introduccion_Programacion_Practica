005368ff56fa6264244a65e539b131d92b69c19a
Aleja-1704 <arojasv2@eafit.edu.co>
2022-04-21 12:53:01 -0500

    Carpeta

---
+++ patronesrepetitivos/patronrepetidoAlejaRojas.py
@@ -0,0 +1,16 @@
+num1=int(input("Ingrese número 1 "))
+num2=int(input("Ingrese número 2 "))
+num3=int(input("Ingrese número 3 "))
+num4=int(input("Ingrese número 4 "))
+num5=int(input("Ingrese número 5 "))
+
+operacion1=(num1*num1)+1
+print("Resultado de la operación 1 es", operacion1)
+operacion2=(num2*num2)+1
+print("Resultado de la operación 2 es",operacion2)
+operacion3=(num3*num3)+1
+print("Resultado de la operación 3 es",operacion3)
+operacion4=(num4*num4)+1
+print("Resultado de la operación 4 es",operacion4)
+operacion5=(num5*num5)+1
+print("Resultado de la operación 5 es",operacion5)
