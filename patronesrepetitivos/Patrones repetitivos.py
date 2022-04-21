272d24560716a7edb85c94a90da4e61cc9f69c09
unknown <mjtorressanchez@eafit.edu.co>
2022-04-21 12:54:10 -0500

    Carpeta patrones repetetivos

---
+++ patronesrepetitivos/Patrones repetitivos.py
@@ -0,0 +1,17 @@
+numero1= int(input("Ingresa un numero 1: "))
+numero2= int(input("Ingresa un numero 2: "))
+numero3= int(input("Ingresa un numero 3: "))
+numero4= int(input("Ingresa un numero 4: "))
+numero5= int(input("Ingresa un numero 5: "))
+
+operacion1=(numero1*numero1)+1
+print("el resultado es:", operacion1)
+operacion2=(numero2*numero2)+1
+print("el resultado es:", operacion2)
+operacion3=(numero3*numero3)+1
+print("el resultado es:", operacion3)
+operacion4=(numero4*numero4)+1
+print("el resultado es:", operacion4)
+operacion5=(numero5*numero5)+1
+print("el resultado es:", operacion5)
+
